d84c0632c0c0c84387718e75c2b12a870967cf4f
Jay Pujara <jaypujara76@gmail.com>
2026-08-19 14:36:22 +0530

    refector: forecast changes

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -7,10 +7,11 @@ import {
 import WeatherIcon from "./WeatherIcon";
 
 function formatDate(dateString) {
-  const date = new Date(dateString);
+  const date = new Date(`${dateString}T00:00:00+05:30`);
 
-  return date.toLocaleDateString("en-US", {
+  return date.toLocaleDateString("en-IN", {
     weekday: "short",
+    timeZone: "Asia/Kolkata",
   });
 }
 
@@ -23,12 +24,12 @@ function Forecast({ daily, unit }) {
         {daily.time.map((date, index) => {
           const maxTemperature = convertTemperature(
             daily.temperature_2m_max[index],
-            unit
+            unit,
           );
 
           const minTemperature = convertTemperature(
             daily.temperature_2m_min[index],
-            unit
+            unit,
           );
 
           const code = daily.weather_code[index];
@@ -40,10 +41,7 @@ function Forecast({ daily, unit }) {
               <h3>{formatDate(date)}</h3>
 
               <div className="forecast-icon">
-                <WeatherIcon
-                  slug={iconSlug}
-                  size={64}
-                />
+                <WeatherIcon slug={iconSlug} size={64} />
               </div>
 
               <p>{getWeatherCondition(code)}</p>
